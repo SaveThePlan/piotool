8f2cc94c05ebf8c3fa92b2981c9f1eec3aa8340f
Joffrey <j.plouvier@airdocsolutions.com>
2015-08-30 11:35:54 +0200

    Contacts#index list current_user contacts

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -19,10 +19,11 @@ RSpec.describe ContactsController, type: :controller do
   end
 
   describe "GET #index" do
-    it "assigns all contacts as @contacts" do
-      contact = create(:contact)
+    it "assigns current_user contacts as @contacts" do
+      user_contact = create(:contact, user: user)
+      other_contact = create(:contact)
       get :index, {}, valid_session
-      expect(assigns(:contacts)).to eq([contact])
+      expect(assigns(:contacts)).to eq([user_contact])
     end
   end
 
