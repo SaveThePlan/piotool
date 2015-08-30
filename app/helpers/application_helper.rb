740e252a036faf742db9b4412f2c567978423772
Joffrey <j.plouvier@airdocsolutions.com>
2015-08-30 11:32:12 +0200

    Add basics styles and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def flash_msg_class type
+    case type.to_sym
+      when :success
+        'alert-success'
+      when :error
+        'alert-danger'
+      when :alert
+        'alert-warning'
+      when :notice
+        'alert-info'
+      else
+        type.to_s
+    end
+  end
+
 end
